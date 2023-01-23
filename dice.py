b38e964f1f4e031fd471be4e1085b2286faad644
XxCryptoSagexX <tre.williams001@gmail.com>
2023-01-23 11:31:51 -0600

    Completed project without Refactoring

--- dice.py
+++ dice.py
@@ -1,5 +1,101 @@
 # dice.py
-# Successful downloade to new PC
+
+import random
+
+DICE_ART = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
+DIE_HEIGHT = len(DICE_ART[1])
+DIE_WIDTH = len(DICE_ART[1][0])
+DIE_FACE_SEPARATOR = " "
+
+def generate_dice_faces_diagram(dice_values):
+    """Return an ASCII diagram of dice faces from `dice_values`.
+
+    The string returned contains an ASCII representation of each die.
+    For example, if `dice_values = [4, 1, 3, 2]` then the string
+    returned looks like this:
+
+    ~~~~~~~~~~~~~~~~~~~ RESULTS ~~~~~~~~~~~~~~~~~~~
+    ┌─────────┐ ┌─────────┐ ┌─────────┐ ┌─────────┐
+    │  ●   ●  │ │         │ │  ●      │ │  ●      │
+    │         │ │    ●    │ │    ●    │ │         │
+    │  ●   ●  │ │         │ │      ●  │ │      ●  │
+    └─────────┘ └─────────┘ └─────────┘ └─────────┘
+    """
+    # Generate a list of dice faces from DICE_ART
+    dice_faces = []
+    for value in dice_values:
+        dice_faces.append(DICE_ART[value])
+
+    # Generate a list containing the dice faces rows
+    dice_faces_rows = []
+    for row_idx in range(DIE_HEIGHT):
+        row_components = []
+        for die in dice_faces:
+            row_components.append(die[row_idx])
+        row_string = DIE_FACE_SEPARATOR.join(row_components)
+        dice_faces_rows.append(row_string)
+    
+    # Generate header with the word "Results" centered 
+    width = len(dice_faces_rows[0])
+    diagram_header = " RESULTS ".center(width, "~")
+
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram 
+
+def roll_dice(num_dice):
+    """ Return a list of integers with length 'num_dice'.
+
+    Each integer in the rerurned list is a random number between
+    1 and 6, inclusive
+    """
+    roll_results = []
+    for _ in range(num_dice):
+        roll = random.randint(1, 6)
+        roll_results.append(roll)
+    return roll_results
 
 def parse_input(input_string):
     """Return `input string` is an integer number between 1-6. 
@@ -14,7 +110,16 @@ def parse_input(input_string):
     else:
         print("Please enter a number from 1 to 6.")
         raise SystemExit(1)
+
+
+
 # ~~~ App's main code block ~~~
 # 1. Get and validate user's input
 num_dice_input = input("How many dice do you want to roll? [1-6] ")
 num_dice = parse_input(num_dice_input)
+# 2. Roll the dice
+roll_results = roll_dice(num_dice)
+# 3. Generate the ASCII Diagram of dice faces
+dice_face_diagram = generate_dice_faces_diagram(roll_results)
+# 4. display the diagram
+print(f"\n{dice_face_diagram}")
